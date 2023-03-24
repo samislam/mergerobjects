fbfc26b4e0f2f45cd66f1986fe04e236060e8ccd
islam yamor <islamyamor@pm.me>
2023-03-24 12:36:20 +0300

    corrected the main prop in package.json and removed source maps for declaration files

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,2 +1,1 @@
 export { default } from './mergeObjects';
-//# sourceMappingURL=index.d.ts.map
--- @types/mergeObjects.d.ts
+++ @types/mergeObjects.d.ts
@@ -1,3 +1,2 @@
 import { UnionToIntersection } from 'utility-types';
 export default function mergeObjects<SRCS extends object[]>(...sources: SRCS): UnionToIntersection<SRCS[number]>;
-//# sourceMappingURL=mergeObjects.d.ts.map
